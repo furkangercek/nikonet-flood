27761f697f1fa1c58e02c4b038951c2a0625f9a3
furkangercek <furkangercek97@gmail.com>
2025-03-21 16:21:08 +0300

    added fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import "./index.css";
 
 function App() {
-  return <div className="text-xl">Welcome User...</div>;
+  return (
+    <div className="bg-purple-300 p-8 text-white">
+      <h1 className="font-terminus">Regular Terminus Font</h1>
+      <h1 className="font-terminus font-bold ">Bold Terminus Font</h1>
+      <h1 className="font-terminus italic ">Italic Terminus Font</h1>
+      <h1 className="font-terminus font-bold italic ">
+        Bold Italic Terminus Font
+      </h1>
+    </div>
+  );
 }
 
 export default App;
